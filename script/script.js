fabc2fe3e0e9f630c728ac15bb35e231e481b9b1
kik094 <79009903+kik094@users.noreply.github.com>
2021-05-31 12:43:10 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,10 @@
+document.getElementById("open-burger").onclick = function (){
+    document.getElementById("show-burger").style.display="block"
+}
+
+document.getElementById("close-burger").onclick = function (){
+    document.getElementById("show-burger").style.display="none"
+}
+
+
+
